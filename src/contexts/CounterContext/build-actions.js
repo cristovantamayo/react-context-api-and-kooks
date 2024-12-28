cc667bc8f3a97ed005dde5996c0af24482d2ab1b
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2024-12-27 21:13:43 -0300

    Context and Hooks

--- src/contexts/CounterContext/build-actions.js
+++ src/contexts/CounterContext/build-actions.js
@@ -4,14 +4,32 @@ export const buildActions = (dispatch) => {
   return {
     increase: () => dispatch({ type: actionTypes.INCREASE }),
     decrease: () => dispatch({ type: actionTypes.DECREASE }),
-    reset: () => dispatch({ type: "CounterContext/RESET" }),
+    reset: () => dispatch({ type: actionTypes.RESET }),
     setCounter: (payload) =>
-      dispatch({ type: "CounterContext/SET_COUNTER", payload }),
-    asyncIncreaseStart: () =>
-      dispatch({ type: "CounterContext/ASYNC_INCREASE_START" }),
-    asyncIncreaseEnd: () =>
-      dispatch({ type: "CounterContext/ASYNC_INCREASE_END" }),
-    asyncIncreaseError: () =>
-      dispatch({ type: "CounterContext/ASYNC_INCREASE_ERROR" }),
+      dispatch({ type: actionTypes.SET_COUNTER, payload }),
+    asyncIncrease: () => asyncIncreaseFn(dispatch),
+    asyncError: () => asyncErrorFn(dispatch),
   };
+};
+
+const asyncIncreaseFn = async (dispatch) => {
+  dispatch({ type: actionTypes.ASYNC_INCREASE_START });
+
+  return await new Promise((r) => {
+    setTimeout(() => {
+      dispatch({ type: actionTypes.ASYNC_INCREASE_END });
+      r("RESOLVED!");
+    }, 2000);
+  });
+};
+
+const asyncErrorFn = async (dispatch) => {
+  dispatch({ type: actionTypes.ASYNC_INCREASE_START });
+
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      dispatch({ type: actionTypes.ASYNC_INCREASE_ERROR });
+      reject(new Error("Deu Ruim!"));
+    }, 2000);
+  });
 };
